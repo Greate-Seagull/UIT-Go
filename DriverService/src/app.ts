f78e1fc5a19b0560a446318a5175fd8aa862dcdc
Hưng <h0274.hmt@gmail.com>
2025-10-30 15:15:26 +0700

    Delete DriverService/src directory

--- DriverService/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import driverRouter from "./presentation/routes/driver.route";
-
-const app = express();
-
-app.use(express.json());
-app.use("/api/drivers", driverRouter);
-
-export default app;
--- DriverService/src/scripts/check-db.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../composition-root";
-
-async function test() {
-	const result = await prisma.driver.findMany();
-	console.log(result);
-}
-
-test();
