e02a9e87e493c6c6f70507bdf630b235ff0718bd
minh10m <minhhh1010@gmail.com>
2025-11-02 11:23:54 +0700

    refactor: missing annotation repository

--- TripService/trip-service/src/main/java/com/uitgo/trip/repo/TripRatingRepository.java
+++ TripService/trip-service/src/main/java/com/uitgo/trip/repo/TripRatingRepository.java
@@ -3,5 +3,7 @@ package com.uitgo.trip.repo;
 
 import com.uitgo.trip.domain.TripRating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TripRatingRepository extends JpaRepository<TripRating, Long> { }
