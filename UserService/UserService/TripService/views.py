ef180a72ee0a163eec7a1f39115df373c7a49c79
Cao Thành Huy <145009627+huypro2005@users.noreply.github.com>
2025-11-02 11:21:35 +0700

    update call api TripService

--- UserService/UserService/TripService/views.py
+++ UserService/UserService/TripService/views.py
@@ -1,28 +1,35 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework import response
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 import requests
 
-URL_TRIPSERVICE = 'http://localhost:8082/'
+URL_TRIPSERVICE = 'http://host.docker.internal:8082/'
 
 class TripServiceViews(APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request):
-        data = request.data 
+        headers = {
+            'X-User-Id': str(request.user.id),
+            'Content-Type': 'application/json'
+        }
         try:
-            res = requests.post(f'{URL_TRIPSERVICE}trips/', data=data)
-            if res.status_code == 200:
-                return response({
-                    'message': 'Create trip success',
-                    'data': res.json()
-                }, status=status.HTTP_200_OK)
-            return response({
-                'message': 'Create trip fail',
-            }, status=status.HTTP_400_BAD_REQUEST)
+            res = requests.post(f'{URL_TRIPSERVICE}trips', json=request.data, headers=headers, timeout=5)
+            data = res.json() if res.content else {}
+
+            if res.ok:
+                return Response(
+                    {'message': 'Create trip success', 'data': data},
+                    status=res.status_code
+                )
+
+            return Response(
+                {'message': 'Create trip failed', 'error': data},
+                status=status.HTTP_400_BAD_REQUEST
+            )
         except Exception as e:
-            return response({
+            return Response({
                 'message': "Has error",
                 'error': f'Error: {e}'
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -33,15 +40,15 @@ class TripServiceDetailView(APIView):
         try:
             res = requests.get(f'{URL_TRIPSERVICE}trips/{trip_id}')
             if res.status_code == 200:
-                return response({
+                return Response({
                     'message': 'Retrieve trip success',
                     'data': res.json()
                 }, status=status.HTTP_200_OK)
-            return response({
+            return Response({
                 'message': 'Retrieve trip fail',
             }, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return response({
+            return Response({
                 'message': "Has error",
                 'error': f'Error: {e}'
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -49,21 +56,47 @@ class TripServiceDetailView(APIView):
 
 class TripCancelView(APIView):
     permission_classes = [IsAuthenticated]
-    def get(self, request, trip_id):
+    def post(self, request, trip_id):
         try:
-            res = requests.get(f'{URL_TRIPSERVICE}trips/{trip_id}/cancel')
+            user_id = request.user.id
+            headers = {'X-User-Id': user_id}
+            res = requests.post(f'{URL_TRIPSERVICE}trips/{trip_id}/cancel', headers=headers)
             if res.status_code == 200:
-                return response({
+                return Response({
                     'message': 'Cancel trip success',
                     'data': res.json()
                 }, status=status.HTTP_200_OK)
-            return response({
+            return Response({
                 'message': 'Cancel trip fail',
             }, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return response({
+            return Response({
                 'message': "Has error",
                 'error': f'Error: {e}'
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
+
+class TripRatingView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request, trip_id):
+        try:
+            user_id = request.user.id
+            headers = {'X-User-Id': user_id}
+            res = requests.post(f'{URL_TRIPSERVICE}trips/{trip_id}/rating', headers=headers)
+            if res.status_code == 200:
+                return Response({
+                    'message': 'Cancel trip success',
+                    'data': res.json()
+                }, status=status.HTTP_200_OK)
+            return Response({
+                'message': 'Cancel trip fail',
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({
+                'message': "Has error",
+                'error': f'Error: {e}'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+
         
