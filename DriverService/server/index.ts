f14fd7ce539cd6e641f9a77bb9d9dd7b0acd3742
Greate-Seagull <h0274.hmt@gmail.com>
2026-01-10 19:47:24 +0700

    feat: export monitoring logs

--- DriverService/server/index.ts
+++ DriverService/server/index.ts
@@ -1,13 +1,15 @@
 import app from "./src/app";
 import { requestQueue } from "./src/infrastructure/request-queue/local.request-queue";
 import { workerLoop } from "./src/infrastructure/request-queue/worker";
+import { startMonitoring } from "./src/presentation/monitoring/monitorings";
 
 const PORT = 3000;
 
+startMonitoring();
 workerLoop(requestQueue);
 
 app.listen(PORT, (error: any) => {
 	if (error) throw error;
-	console.log(`Version 1.0.1`);
+	console.log(`Version 1.0.4`);
 	console.log(`Listening to http://localhost:${PORT}`);
 });
